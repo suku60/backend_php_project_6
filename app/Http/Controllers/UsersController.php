5c242bc80bfaf189ad529111579f16a8c6fecac8
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-04-14 17:06:10 +0200

    Basic log functions added to UsersController.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,13 +1,88 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\User;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class UsersController extends Controller
 {   
+    public function addUser(Request $request)
+    {
+        Log::info('addUser()');
+
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|max:14',
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(),400);
+        }
+
+        $user = User::create([
+            'username' => $request->get('username'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+    public function logUser(Request $request)
+    {
+        Log::info('logUser()');
+
+        $input = $request->only('username', 'password');
+        $jwt_token = null;
+
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Wrong password or username',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $jwt_token,
+        ]);
+    }
+
+    public function showMyUser()
+    {
+        Log::info('showMyUser()');
+
+       return response()->json(auth()->user());;
+    }
+
+    public function logOutUser(Request $request)
+    {
+        Log::info('logOutUser()');
+      
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        try {
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Logged out'
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unexpected error logging out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
     
     // register & login functions will be developed at authcontroller later
     // logs will be deleted later after checking
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('username', 14)->unique();;
+            $table->string('steamUsername', 50)->nullable();
             $table->string('email', 100)->unique();
             $table->string('password', 20);
-            $table->string('role', 10);
-            $table->string('steamUsername', 50)->nullable();
+            $table->string('role', 10)->nullable();
             $table->timestamps();
         });
     }
